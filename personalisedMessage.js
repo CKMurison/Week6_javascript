37c76223bdd6f4a6bb392c46742351a8ca2216ce
Cameron Murison <cmuris4@gmail.com>
2023-05-24 11:44:18 +0100

    challenge for array operations complete

--- personalisedMessage.js
+++ personalisedMessage.js
@@ -1,8 +1,14 @@
-const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
+// An array of objects!
+const namesAndDiscounts = [
+  { name: 'Anna', discount: 50 },
+  { name: 'Laura', discount: 40 },
+  { name: 'Josh', discount: 30 },
+  { name: 'Min', discount: 50 },
+  { name: 'Karla', discount: 60 }
+];
 
-const hiNames = names.map((name) => {
-  return `Hi ${name} 50% off our best candies for you today!`
+const generateMessage = namesAndDiscounts.map(({ name, discount }) => {
+  return `Hi ${name}, ${discount}% off our best candies today!`
 });
 
-
-console.log(hiNames)
+console.log(generateMessage)
