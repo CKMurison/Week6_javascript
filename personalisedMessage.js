40dc3524e00bf63c77e8e022bd7a27f4a2a7513e
Cameron Murison <cmuris4@gmail.com>
2023-05-24 11:38:08 +0100

    personalised message mapping over array

---
+++ personalisedMessage.js
@@ -0,0 +1,8 @@
+const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
+
+const hiNames = names.map((name) => {
+  return `Hi ${name} 50% off our best candies for you today!`
+});
+
+
+console.log(hiNames)
