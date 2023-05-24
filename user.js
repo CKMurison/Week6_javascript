2e002d50f74a1792fa1e6a50fa820462ee783c3d
Cameron Murison <cmuris4@gmail.com>
2023-05-24 13:41:37 +0100

    exercise2 for classes

--- user.js
+++ user.js
@@ -14,4 +14,40 @@ class User {
   }
 }
 
+const users = [
+  new User('Uma'),
+  new User('Josh'),
+  new User('Ollie')
+];
+
+class UserBase {  
+  
+  constructor(users) {
+  this.users = users;
+  }
+
+  count() {
+    return this.users.length;
+  }
+
+  getNames() {
+    this.users.forEach(user => {
+      console.log(user.name);
+  });
+}
+
+  getIntroduction() {
+    this.users.forEach(user => {
+      console.log('Hey my name is ' + user.name);
+  });
+}
+}
+
+let base = new UserBase(users);
+
+console.log(base.count())
+console.log(base.getNames())
+console.log(base.getIntroduction())
+
+
 module.exports = User;
