4c5234f154f1601ac7e9c8c39efbbaeb2960868e
Cameron Murison <cmuris4@gmail.com>
2023-05-26 15:11:10 +0100

    thermostat with 1 broken test. error when raising temp past 25 with powersaver on

--- my_project/thermostat.js
+++ my_project/thermostat.js
@@ -1,11 +1,46 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.powerSaving = false;
   }
 
   getTemp() {
   return this.temperature;
   }
+
+  up() {
+    if (this.powerSaving == true && this.temperature == 25) {
+      return 'Sorry. Max temperature reached! Turn off power saving to go even hotter!'
+    } else {
+    this.temperature = this.temperature + 1;
+    }
+  }
+
+  down() {
+    this.temperature = this.temperature - 1;
+  }
+
+  reset() {
+    this.temperature = 20
+  }
+
+  setPowerSaving(boolean) {
+    if (boolean == true) {
+      this.powerSaving == true;
+      return 'Power Saver on! The max temp is now 25'
+    } else {
+      this.powerSaving == false
+      return 'Power saver off! Careful you don/t melt!'
+    }
+  }
 }
 
 module.exports = Thermostat;
+
+
+// min possible temp === 10
+
+// setPowerSaving mode which takes a boolean as argument 
+// and set temp max to 25 
+
+// if setPowerSaving(false), max temp === 32
--- my_project/thermostat.test.js
+++ my_project/thermostat.test.js
@@ -5,4 +5,41 @@ describe('Thermostat', () => {
     const response = new Thermostat
     expect(response.getTemp()).toEqual(20);
   });
+  it('should raise the temp by 1 degree', () => {
+    const response = new Thermostat
+    response.up();
+    expect(response.getTemp()).toEqual(21);
+  });
+  it('should decrease the temp by 1', () => {
+    const response = new Thermostat
+    response.down();
+    expect(response.getTemp()).toEqual(19);
+  });
+  it('should reset the temperature to 20', () => {
+    const response = new Thermostat
+    response.up();
+    response.up();
+    response.reset();
+    expect(response.getTemp()).toEqual(20)
+  });
+  it('should turn on power saving mode which sets the max temp to 25', () => {
+    const response = new Thermostat
+    response.setPowerSaving();
+    response.up();
+    response.up();
+    response.up();
+    response.up();
+    response.up();
+    expect(response.getTemp()).toEqual(25)
+  });
+  it('should not surpass 25 degrees if power saver is on', () => {
+    const response = new Thermostat
+    response.setPowerSaving();
+    response.up();
+    response.up();
+    response.up();
+    response.up();
+    response.up();
+    expect(response.up()).toEqual('Sorry. Max temperature reached! Turn off power saving to go even hotter!')
+  })
 });
